d576c6de878299f11f6e43aa9747d507e8643914
Noah Luck Easterly <noah.easterly@gmail.com>
2019-06-23 12:22:17 -0400

    Add Kind.Concrete.Identity

---
+++ src/Kind/Concrete/Identity.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+module Kind.Concrete.Identity where
+import qualified Data.Functor.Identity as Shadowed
+
+type family Identity = (i :: k -> k) | i -> k where
+  Identity = Shadowed.Identity
+  Identity = Identity_
+
+data family Identity_ (a :: j -> k) :: j -> k
+newtype instance Identity_ a x = Identity1 { runIdentity1 :: a x }
+newtype instance Identity_ a x y = Identity2 { runIdentity2 :: a x y }
+newtype instance Identity_ a x y z = Identity3 { runIdentity3 :: a x y z }
