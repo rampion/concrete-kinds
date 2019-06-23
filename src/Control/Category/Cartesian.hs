2e095c8c8d1f8cec5cfef9b8918c5711e61f4341
Noah Luck Easterly <noah.easterly@gmail.com>
2019-06-23 09:34:22 -0400

    Add Control.Category.Cartesian

---
+++ src/Control/Category/Cartesian.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TypeOperators #-}
+module Control.Category.Cartesian where
+import Control.Category.Strong
+import qualified Control.Arrow as Shadowed
+import qualified Prelude as Shadowed
+
+class Strong m => Cartesian m where
+  fst :: Product m a b `m` a
+  snd :: Product m a b `m` b
+  (&&&) :: c `m` a -> c `m` b -> c `m` Product m a b
+  infixr 3 &&&
+
+instance Cartesian (->) where
+  fst = Shadowed.fst
+  snd = Shadowed.snd
+  (&&&) = (Shadowed.&&&)
