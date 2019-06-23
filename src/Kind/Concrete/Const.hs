7a0e80b9749daaed3c0e5d0569d7117a04aeebae
Noah Luck Easterly <noah.easterly@gmail.com>
2019-06-23 12:23:28 -0400

    Add Kind.Concrete.Const

---
+++ src/Kind/Concrete/Const.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+module Kind.Concrete.Const where
+import qualified Data.Functor.Const as Shadowed
+
+type family Const = (c :: k -> j -> k) | c -> k where
+  Const = Shadowed.Const
+  Const = Const_
+
+data family Const_ (a :: j -> k) (b :: i) :: j -> k
+newtype instance Const_ a b x = Const1 { getConst1 :: a x }
+newtype instance Const_ a b x y = Const2 { getConst2 :: a x y }
+newtype instance Const_ a b x y z = Const3 { getConst3 :: a x y z }
