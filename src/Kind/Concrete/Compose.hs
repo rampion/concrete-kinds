094292c9461370f7eb9a11698c4be9fc584de30c
Noah Luck Easterly <noah.easterly@gmail.com>
2019-06-26 07:42:47 -0400

    Add Kind.Concrete.Compose

---
+++ src/Kind/Concrete/Compose.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE TypeOperators #-}
+module Kind.Concrete.Compose where
+import Data.Functor.Compose
+
+type family (∘) = (o :: (j -> k) -> (i -> j) -> i -> k) | o -> k where
+  (∘) = Compose
+  (∘) = Compose_
+infixr 9 ∘
+
+data family Compose_ (f :: i -> j -> k) (g :: h -> i) (a :: h) :: j -> k
+newtype instance Compose_ f g a x = Compose1 { getCompose1 :: f (g a) x }
+newtype instance Compose_ f g a x y = Compose2 { getCompose2 :: f (g a) x y }
+newtype instance Compose_ f g a x y z = Compose3 { getCompose3 :: f (g a) x y z }
