337a76fdc58aff7dc70b375c487def9e0e645944
Noah Luck Easterly <noah.easterly@gmail.com>
2020-08-16 06:10:26 -0400

    split off profunctor into own module

--- src/Control/Categorical/Bifunctor.hs
+++ src/Control/Categorical/Bifunctor.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE QuantifiedConstraints #-}
 module Control.Categorical.Bifunctor where
 
 import Prelude hiding (id, (.), Functor(..))
@@ -58,47 +57,3 @@ instance
     , Birepresentational arrow
     , Category arrow
     ) => Bifunctor bifunctor firstarrow secondarrow arrow
-
-class 
-    ( forall q. Functor (Flipped profunctor q) (Flipped leftarrow) arrow
-    , forall p. Functor (profunctor p) rightarrow arrow
-    , Flippable profunctor
-    , Flippable leftarrow
-    , Category arrow
-    , Birepresentational arrow
-    ) => Profunctor (profunctor :: leftobject -> rightobject -> object)
-                    (leftarrow :: leftobject -> leftobject -> *)
-                    (rightarrow :: rightobject -> rightobject -> *)
-                    (arrow :: object -> object -> *) where
-
-  lmap :: a `leftarrow` b -> profunctor b x `arrow` profunctor a x
-  default lmap :: ( rotcnuforp ~ Flipped profunctor
-                  , worratfel ~ Flipped leftarrow
-                  , forall q. Functor (rotcnuforp q) worratfel arrow
-                  )
-               => a `leftarrow` b -> profunctor b x `arrow` profunctor a x
-  lmap = contrapmap
-
-  rmap :: c `rightarrow` d -> profunctor x c `arrow` profunctor x d
-  rmap = qmap
-
-  dimap :: a `leftarrow` b -> c `rightarrow` d -> profunctor b c `arrow` profunctor a d
-  dimap f g = lmap @_ @_ @_ @_ @_ @rightarrow f
-            . rmap @_ @_ @_ @_ @leftarrow @_ g
-
-instance
-    ( rotcnuforp ~ Flipped profunctor
-    , worratfel ~ Flipped leftarrow
-    , forall q. Functor (rotcnuforp q) worratfel arrow
-    , forall p. Functor (profunctor p) rightarrow arrow
-    , Flippable profunctor
-    , Flippable leftarrow
-    , Birepresentational arrow
-    , Category arrow
-    ) => Profunctor profunctor leftarrow rightarrow arrow
-
-profunctor_example :: Int -> String
-profunctor_example = dimap toEnum (replicate 5) succ
-
-profunctor_example' :: Profunctor profunctor (->) (->) (->) => profunctor String Int -> profunctor Int String
-profunctor_example' = dimap (replicate <*> toEnum) (replicate <*> toEnum)
--- src/Control/Categorical/Profunctor.hs
+++ src/Control/Categorical/Profunctor.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DefaultSignatures #-}
--- {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
--- {-# LANGUAGE UndecidableInstances #-}
--- prevent GHC from complaining about Profunctor
-{-# OPTIONS_GHC -Wno-simplifiable-class-constraints #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Categorical.Profunctor where
 
 import Prelude hiding (id, (.), Functor(..))
@@ -17,34 +17,47 @@ import Control.Category
 
 import Control.Categorical.Functor
 import Data.Dual
-import Data.Universal
 
-class ( Functor (Dual leftarrow) (Universal arrow) profunctor 
-      , Functor rightarrow (Universal arrow) (Dual profunctor)
-      , Category arrow
-      ) => Profunctor (leftarrow :: leftobject -> leftobject -> *)
-                      (rightarrow :: rightobject -> rightobject -> *)
-                      (arrow :: object -> object -> *)
-                      (profunctor :: leftobject -> rightobject -> object) where
+class 
+    ( forall q. Functor (Flipped profunctor q) (Flipped leftarrow) arrow
+    , forall p. Functor (profunctor p) rightarrow arrow
+    , Flippable profunctor
+    , Flippable leftarrow
+    , Category arrow
+    , Birepresentational arrow
+    ) => Profunctor (profunctor :: leftobject -> rightobject -> object)
+                    (leftarrow :: leftobject -> leftobject -> *)
+                    (rightarrow :: rightobject -> rightobject -> *)
+                    (arrow :: object -> object -> *) where
+
   lmap :: a `leftarrow` b -> profunctor b x `arrow` profunctor a x
-  default lmap
-    :: HasDual leftobject
-    => a `leftarrow` b -> profunctor b x `arrow` profunctor a x
+  default lmap :: ( rotcnuforp ~ Flipped profunctor
+                  , worratfel ~ Flipped leftarrow
+                  , forall q. Functor (rotcnuforp q) worratfel arrow
+                  )
+               => a `leftarrow` b -> profunctor b x `arrow` profunctor a x
   lmap = contrapmap
 
   rmap :: c `rightarrow` d -> profunctor x c `arrow` profunctor x d
-  default rmap
-    :: ( HasDual object
-       , Birepresentational arrow
-       )
-    => c `rightarrow` d -> profunctor x c `arrow` profunctor x d
   rmap = qmap
 
-  dimap
-    :: a `leftarrow` b
-    -> c `rightarrow` d
-    -> profunctor b c `arrow` profunctor a d
-  dimap f g = lmap @_ @_ @_ @_ @rightarrow f
-            . rmap @_ @_ @_ @leftarrow g
+  dimap :: a `leftarrow` b -> c `rightarrow` d -> profunctor b c `arrow` profunctor a d
+  dimap f g = lmap @_ @_ @_ @_ @_ @rightarrow f
+            . rmap @_ @_ @_ @_ @leftarrow @_ g
 
-instance Profunctor (->) (->) (->) (->)
+instance
+    ( rotcnuforp ~ Flipped profunctor
+    , worratfel ~ Flipped leftarrow
+    , forall q. Functor (rotcnuforp q) worratfel arrow
+    , forall p. Functor (profunctor p) rightarrow arrow
+    , Flippable profunctor
+    , Flippable leftarrow
+    , Birepresentational arrow
+    , Category arrow
+    ) => Profunctor profunctor leftarrow rightarrow arrow
+
+profunctor_example :: Int -> String
+profunctor_example = dimap toEnum (replicate 5) succ
+
+profunctor_example' :: Profunctor profunctor (->) (->) (->) => profunctor String Int -> profunctor Int String
+profunctor_example' = dimap (replicate <*> toEnum) (replicate <*> toEnum)
